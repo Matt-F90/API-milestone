974fdab86550788f0671d8d8080e3fdad245d98e
Ubuntu <ubuntu@ip-172-31-85-168.ec2.internal>
2019-11-18 18:01:30 +0000

    bootstrap responivity and wire frame

--- js/api.js
+++ js/api.js
@@ -48,7 +48,7 @@
                  //display result's number
                 document.getElementById('output').innerHTML = '<a>TOTAL RESULT: </a>' + total + '<br><br><a>LISTED RESULT: 1</a>';
                 //display first result
-                document.getElementById('searchobj').innerHTML = '<div id="main_container"><div id="img_container"><img src="https://image.tmdb.org/t/p/w200' + poster_path_p + '"></div><div id="text_container"><div class="test"><h1>' + title_p + '</h1></div>' + '<p id="overview">' + overview_p + '</p>' + '<p id="release_date">' + release_date_p +'</p></div></div>';
+                document.getElementById('searchobj').innerHTML = '<div id="main_container"><div class="row"><div class="col-12 col-md-4><div id="img_container"><img src="https://image.tmdb.org/t/p/w200' + poster_path_p + '"></div></div></div><div class="row"><div class="col-12 col-md-4><div id="text_container"></div></div><h1>' + title_p + '</h1></div>' + '<p id="overview">' + overview_p + '</p>' + '<p id="release_date">' + release_date_p +'</p></div></div>';
                
                 
             })
